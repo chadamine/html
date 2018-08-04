5e9846da9377975b877f4e7d0909a7c59591e565
chadamine <chadwicklew@gmail.com>
2018-08-04 04:56:15 -0600

    start bar chart

--- script.js
+++ script.js
@@ -30,6 +30,7 @@ slider3.oninput = function() {
   adjust(3);
 }
 
+// change oil values
 function adjust(num) {
 
   var val1 = 0;
@@ -97,9 +98,73 @@ function adjust(num) {
   }
 }
 
+// round results to 100ths
 function round(number) {
-	return Math.round(number * 100) / 100
+	return Math.round(number * 100) / 100;
 }
+
+//simplest barchart
+var c = document.getElementById("results_canvas");
+var ctx = c.getContext("2d");
+ctx.fillRect(0,0,40,100);
+ctx.fillRect(45,0,40,75);
+ctx.fillRect(90, 0, 40, 50);
+
+// BARCHART
+var resultsCanvas = document.getElementById("myCanvas");
+resultsCanvas.width = 300;
+resultsCanvas.height = 300;
+
+var ctx = resultsCanvas.getContext("2d");
+
+function drawLine(ctx, startX, startY, endX, endY, color) {
+  ctx.save();
+  ctx.strokeStyle = color;
+  ctx.beginPath();
+  ctx.moveTo(startX, startY);
+  ctx.lineTo(endX, endY);
+  ctx.stroke();
+  ctx.restore();
+}
+
+var Barchart = function(options) {
+	this.options = options;
+	this.canvas = options.canvas;
+	this.ctx = this.canvas.getContext("2d");
+	this.colors = options.colors;
+
+	this.draw = function(){
+		var maxValue = 0;
+		for(var categ in this.options.data){
+			maxValue = Math.max(maxValue, this.options.data[categ]);
+		}
+		var canvasActualHeight = this.canvas.height - this.options.padding * 2;
+		var canvastActualWidth = this.canvas.width = this.options.padding * 2;
+    
+		var barIndex = 0;
+		var barQty = object.keys(this.options.data).length;
+		var barSize = (canvasActualWidth)/numberOfBars;
+
+		for (categ in this.options.data) {
+			var val = this.options.data[categ];
+			var barHeight = Math.round(canvasActualHeight * val/maxValue);
+			drawBar(this.ctx,
+			this.options.padding + barIndex * barSize,
+			this.canvas.height - barHeight - this.options.padding,
+			barSize, 
+			barHeight,
+			this.colors[barINdex%this.colors.length]);
+
+			barIndex++;
+		}
+
+	}
+}
+
+var resultsBarchart = new Barchart({
+	canvas:resultsCanvas,
+	colors:["#a55ca5", "#67b6c7", "bccd7a", "#eb9743"]
+});
 
 // Coconut
 //
